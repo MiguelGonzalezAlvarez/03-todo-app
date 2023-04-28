3d8d94cb0cd70ed98078e96098a02c43f512b07c
MiguelMuros <miggonzalv@gmail.com>
2023-04-28 14:13:36 +0200

    feat: create first componentes for todo app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoAddComponent } from './todos/todo-add/todo-add.component';
+import { TodoFooterComponent } from './todos/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todos/todo-item/todo-item.component';
+import { TodoListComponent } from './todos/todo-list/todo-list.component';
+import { TodoModule } from './todos/todo.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todos/todo.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoPageComponent } from './todo-page/todo-page.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
+@NgModule({
+  declarations: [
+    TodoPageComponent,
+    TodoAddComponent,
+    TodoFooterComponent,
+    TodoItemComponent,
+    TodoListComponent,
+    TodoPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
