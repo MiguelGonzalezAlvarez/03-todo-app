eb2ae80f638df4904105ea094e0380adb2a4f610
MiguelMuros <miggonzalv@gmail.com>
2023-05-02 09:24:22 +0200

    chore: move components to a new folder to distinguish from redux

--- src/app/todos/todo.module.ts
+++ src/app/todos/todo.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TodoPageComponent } from './todo-page/todo-page.component';
-import { TodoAddComponent } from './todo-add/todo-add.component';
-import { TodoFooterComponent } from './todo-footer/todo-footer.component';
-import { TodoItemComponent } from './todo-item/todo-item.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoAddComponent } from './todo-components/todo-add/todo-add.component';
+import { TodoFooterComponent } from './todo-components/todo-footer/todo-footer.component';
+import { TodoItemComponent } from './todo-components/todo-item/todo-item.component';
+import { TodoListComponent } from './todo-components/todo-list/todo-list.component';
+import { TodoPageComponent } from './todo-components/todo-page/todo-page.component';
+
 
 @NgModule({
   declarations: [
